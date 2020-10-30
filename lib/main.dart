049c3db18640fe5243773a6a425e509303292946
Rivaan Ranawat <namanrivaan@gmail.com>
2020-10-30 15:22:24 +0530

    Creating random bars and visualising Bubble Sort

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,139 @@
+import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<int> _numbers = [];
+  int _sampleSize = 500;
+  StreamController<List<int>> _streamController;
+  Stream<List<int>> _stream;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  _randomise() {
+    _numbers = [];
+    for (int i = 0; i < _sampleSize; ++i) {
+      _numbers.add(Random().nextInt(_sampleSize));
+    }
+    _streamController.add(_numbers);
+  }
+
+  _bubbleSort()async  {
+    for (int i = 0; i < _numbers.length; ++i) {
+      for (int j = 0; j < _numbers.length - i - 1; ++j) {
+        if (_numbers[j] > _numbers[j + 1]) {
+          int temp = _numbers[j];
+          _numbers[j] = _numbers[j + 1];
+          _numbers[j + 1] = temp;
+        }
+        await Future.delayed(Duration(microseconds: 500));
+        _streamController.add(_numbers);
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _streamController = StreamController<List<int>>();
+    _stream = _streamController.stream;
+    _randomise();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Bubble Sort"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        child: StreamBuilder<Object>(
+          stream: _stream,
+          builder: (context, snapshot) {
+            int counter = 0;
+            return Row(
+              children: _numbers.map((int number) {
+                counter++;
+                return CustomPaint(
+                  painter: BarPainter(
+                    width: 2.0,
+                    value: number,
+                    index: counter,
+                  ),
+                );
+              }).toList(),
+            );
+          }
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              child: Text("Randomise",),
+              onPressed: _randomise,
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Text("Sort",),
+              onPressed: _bubbleSort,
+            ),
+          ),
+        ],
+      ),
     );
+  }
+}
+
+class BarPainter extends CustomPainter {
+  final double width;
+  final int value;
+  final int index;
+
+  BarPainter({this.width, this.value, this.index});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint();
+    if (this.value < 500 * 0) {
+      paint.color = Color.fromRGBO(0, 0, 66, 0.75);
+    } else if (this.value < 500 * .50) {
+      paint.color = Color.fromRGBO(17, 104, 217, 0.75);
+    } else if (this.value < 500 * .75) {
+      paint.color = Color.fromRGBO(0, 217, 159, 0.75);
+    } else if (this.value < 500 * .90) {
+      paint.color = Color.fromRGBO(0, 190, 218, 0.75);
+    }
+    paint.strokeWidth = width;
+    paint.strokeCap = StrokeCap.round;
+    canvas.drawLine(Offset(index * width, 0),
+        Offset(index * width, value.ceilToDouble()), paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
